959cd1b3d504aaf400b882b3b422368d72dd820f
AntoineQuarroz <antoinequarroz.mediam@gmail.com>
2023-04-20 11:30:29 +0200

    Server ON

---
+++ src/Entity/Material.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MaterialRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MaterialRepository::class)]
+class Material
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $titre = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $badge = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $salle = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $localite = null;
+
+    #[ORM\Column]
+    private ?int $nombre = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $identifiant = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $idClass = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getBadge(): ?string
+    {
+        return $this->badge;
+    }
+
+    public function setBadge(string $badge): self
+    {
+        $this->badge = $badge;
+
+        return $this;
+    }
+
+    public function getSalle(): ?string
+    {
+        return $this->salle;
+    }
+
+    public function setSalle(string $salle): self
+    {
+        $this->salle = $salle;
+
+        return $this;
+    }
+
+    public function getLocalite(): ?string
+    {
+        return $this->localite;
+    }
+
+    public function setLocalite(string $localite): self
+    {
+        $this->localite = $localite;
+
+        return $this;
+    }
+
+    public function getNombre(): ?int
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(int $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getIdentifiant(): ?string
+    {
+        return $this->identifiant;
+    }
+
+    public function setIdentifiant(string $identifiant): self
+    {
+        $this->identifiant = $identifiant;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getIdClass(): ?string
+    {
+        return $this->idClass;
+    }
+
+    public function setIdClass(string $idClass): self
+    {
+        $this->idClass = $idClass;
+
+        return $this;
+    }
+}
